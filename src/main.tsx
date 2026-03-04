0da99621a1d0890728484bd93ce8476c98a3a5e8
dorbin.dev <dorbin.dev008@gmail.com>
2026-03-04 22:28:32 +0100

    Set up the workspace, installed dependencies/libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import emailJS from '@emailjs/browser'
 import './index.css'
 import App from './App.tsx'
+
+emailJS.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
